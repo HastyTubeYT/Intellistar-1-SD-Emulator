53beccd63bc6ac459861048552d277be196a22b4
HastyTubeYT <94996367+HastyTubeYT@users.noreply.github.com>
2021-11-24 16:36:48 -0500

    Create narration.js

---
+++ Assets/Narrations/narration.js
@@ -0,0 +1,20 @@
+narrate file: play file:
+  follow weather.js
+Blowing Dust.ogg
+BlowingSnow.ogg
+BlowingSnowNearby.ogg
+Clear.ogg
+Cloudy.ogg
+Drizzle.ogg
+Fair.ogg
+Fog.ogg
+Frezzing Drizzle.ogg
+Frezzing Rain.ogg
+Haze.ogg
+Heavy Rain.ogg
+Heavy Thunderstorm.ogg
+Locate-Users/Username/This PC/Downloads/Assets/Narrations/Weather
+   :[
+     
+     *Amy errors will end up here(
+     [WARN] Cloudy.ogg file not found
